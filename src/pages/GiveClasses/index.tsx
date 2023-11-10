d7a7253e21e1db0f5f950b3a970bdbf7c9328fbb
ThataBispo <thauane05.bs@gmail.com>
2023-11-10 15:44:34 -0300

    prof

--- src/pages/GiveClasses/index.tsx
+++ src/pages/GiveClasses/index.tsx
@@ -1,11 +1,29 @@
-import { View, ImageBackground } from "react-native";
+import { View, ImageBackground, Text, TouchableOpacity } from "react-native";
+import { RectButton } from "react-native-gesture-handler";
+
 import giveClassesBgImage from "../../assets/images/give-classes-background.png";
 import { styles } from "./styles";
+import { useNavigation } from "@react-navigation/native";
 
 export function GiveClasesses() {
+  const navigation = useNavigation();
   return (
     <View style={styles.container}>
-      <ImageBackground source={giveClassesBgImage}></ImageBackground>
+      <ImageBackground
+        resizeMode="contain"
+        source={giveClassesBgImage}
+        style={styles.content}
+      >
+        <Text style={styles.title}>Quer dar aulas?</Text>
+        <Text style={styles.description}>
+          Para começar, você precisa se cadastrar como professor na nossa
+          plataforma web.
+        </Text>
+      </ImageBackground>
+
+      <TouchableOpacity style={styles.okButton}>
+        <Text style={styles.okButtonText}>Vamos lá</Text>
+      </TouchableOpacity>
     </View>
   );
 }
